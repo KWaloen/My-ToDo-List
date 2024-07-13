6e96a7b608b3acf6d8f2b66ce194588f4055a7f6
Kai.Waloen <kawal5842@uib.no>
2024-07-13 20:41:51 +0200

    23:47 in video

--- README.md
+++ README.md
@@ -34,3 +34,6 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+This ToDo List was created based on this tutorial with some of my own personal touches with the goal to learn React: 
+https://www.youtube.com/watch?v=Rh3tobg7hEo
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,63 @@
+//use client is necessary for NextJS to not throw a hissy fit
+"use client"
+
+import { useState } from "react"
+
+
 
 export default function Home() {
-  return( 
-  <div>
-    <form className = "new-item-form"> 
-      <div className = "form-row">
-        <label htmlFor = "item">New Item</label>
-        <input type = "text" id = "item"/>
-      </div>
-      <button className = "btn">Add</button>
-    </form>
-    <h1 className = "header">Todo List</h1>
+  
+  const [newItem, setNewItem] = useState("")
+  const [todos, setTodos] = useState<{ id: number; title: string; completed: boolean; }[]>([])
 
-    <ul className = "list">
-      <li>
-        <label>
-          <input type = "checkbox"/>
+  function handleSubmit(item: React.FormEvent<HTMLFormElement>) {
+    //prevents refreshing the page after clicking the button
+    item.preventDefault();
+
+    setTodos((currentTodos) => {
+      return [
+      ...currentTodos, 
+      {id: currentTodos.length, title: newItem, completed: false},
+      ]
+    })
+  }
+  
+  console.log(todos)
+
+  return (
+    //upon clicking the button, the onSubmit event listener triggers the handleSubmit method
+    <div> 
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input 
+          value={newItem}
+          //this allows the text field to be updated 
+          onChange={item => setNewItem(item.target.value)} 
+          type="text" 
+          id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+
+      <ul className="list">
+        <li>
+          <label>
+            <input type="checkbox" />
             Item 1
-        </label>
-        <button className = "btn btn-danger">Delete</button>
-      </li>
-      <li>
-        <label>
-          <input type = "checkbox"/>
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
+        <li>
+          <label>
+            <input type="checkbox" />
             Item 2
-        </label>
-        <button className = "btn btn-danger">Delete</button>
-      </li>
+          </label>
+          <button className="btn btn-danger">Delete</button>
+        </li>
 
-    </ul>
-  </div>
+      </ul>
+    </div>
   )
 }
